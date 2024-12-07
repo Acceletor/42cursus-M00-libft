7dcb40a7796700b2042595c4b93719b0dc698782
Karintorn Suebthawilkul <karinsueb@gmail.com>
2024-12-07 23:45:55 +0100

    add ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,38 @@
+#include "libft.h"
+
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+    size_t i;
+    size_t j;
+
+    if (*needle == '\0')
+        return ((char *)haystack);
+    i = 0;
+    while (haystack[i] && i < len)
+    {
+        j = 0;
+        while(needle[j] && haystack[i + j] == needle[j] && (i + j) < len)
+            j++;
+        if (needle[j] == '\0')
+            return ((char *)haystack + i);
+        i++;
+    }
+    return (0);
+}
+
+//int main() {
+//    const char	*haystack = "Hello, World!";
+//	const char	*needle = "World";
+//	size_t		len = 13;
+//
+//	char *result = ft_strnstr(haystack, needle, len);
+//	if (result)
+//		printf("Found '%s' at position: %ld\n", needle, result - haystack);
+//	else
+//		printf("Substring not found within the first %zu characters.\n", len);
+//
+//	result = ft_strnstr(haystack, "", len);
+//	printf("Result with empty needle: %p\n", result);
+//
+//	return (0);
+//}
