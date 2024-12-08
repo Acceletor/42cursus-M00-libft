1131ceea5b8f85005c0ca314d31399f54a1b0a4c
Karintorn Suebthawilkul <karinsueb@gmail.com>
2024-12-08 17:35:49 +0100

    add ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+char *ft_strdup(const char *s1)
+{
+    char *des;
+    int len;
+    int i;
+
+    len = ft_strlen(s1);
+    des = malloc(len + 1);
+    if(des == 0)
+        return (0);
+    i = 0;
+    while(s[i])
+    {
+        des[i] = s1[i];
+        i++;
+    }
+    des[i] = 0;
+    return(des);
+}
+
+//int main() {
+//    const char *original = "Hello, World!";
+//    char *duplicate = ft_strdup(original);
+//
+//    if (duplicate == NULL) {
+//        printf("Memory allocation failed\n");
+//        return 1;
+//    }
+//
+//    printf("Original: %s\n", original);
+//    printf("Duplicate: %s\n", duplicate);
+//
+//    free(duplicate); // Free the allocated memory
+//    return 0;
+//}
