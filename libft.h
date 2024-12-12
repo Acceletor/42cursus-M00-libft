e44787f87f41daf3f54ab895be36b7edfaeb49fb
Karintorn Suebthawilkul <ksuebtha@c2r3s2.42warsaw.pl>
2024-12-12 17:43:36 +0100

    add ft_strmapi.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ksuebtha <ksuebtha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 11:45:50 by ksuebtha          #+#    #+#             */
-/*   Updated: 2024/12/12 16:23:49 by ksuebtha         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:08:11 by ksuebtha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,4 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
-
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
